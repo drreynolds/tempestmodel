11933dc42526d4c7c06511e309b4ce730d1d0410
David J. Gardner <gardner48@llnl.gov>
2017-03-28 18:30:06 -0700

    clean up white space

--- src/atm/TempestInitialize.h
+++ src/atm/TempestInitialize.h
@@ -251,7 +251,7 @@ void _TempestSetupMethodOfLines(
 		model.SetTimestepScheme(
 			new TimestepSchemeARK232(model));
 
-		} else if (vars.strTimestepScheme == "gark2") {
+	} else if (vars.strTimestepScheme == "gark2") {
 		model.SetTimestepScheme(
 			new TimestepSchemeGARK2(model));
 
